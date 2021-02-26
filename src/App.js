6e14db39d5b3d30b5ee6882cb1d07201e3bda0dd
Asanka Piyasekara <asankap@shootsta.com>
2021-02-26 16:52:56 +0800

    user mirage for fake-usrs api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
+import { createServer } from "miragejs"
 import './App.css';
 
-function App() {
+const urlPrefix = 'https://jsonplaceholder.typicode.com';
+
+createServer({
+  routes() {
+    this.urlPrefix = urlPrefix;
+    // this.namespace = "/api";
+    this.get("fake-users", () => [
+      { id: "1", name: "Luke" },
+      { id: "2", name: "Leia" },
+      { id: "3", name: "Anakin" },
+    ]);
+    this.passthrough();
+  },
+})
+
+export function App() {
+  let [users, setUsers] = useState([])
+  let [fakeUsers, setFakeUsers] = useState([])
+
+  useEffect(() => {
+    // Querying real users
+    fetch(`${urlPrefix}/users`)
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+
+    // Querying fake users
+    fetch(`${urlPrefix}/fake-users`)
+      .then((response) => response.json())
+      .then((json) => setFakeUsers(json))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* Displaying real users */}
+      <div><strong>Real Users</strong></div>
+      <ul>
+      {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      {/* Displaying fake users */}
+      <div><strong>Fake Users</strong></div>
+      <ul>
+      {fakeUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default App;
